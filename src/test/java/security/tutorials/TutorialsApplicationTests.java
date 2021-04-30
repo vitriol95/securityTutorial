dfc13dff616b77de8730823139702c75c24c2474
vitriol <vitriol95@naver.com>
2021-04-30 14:50:56 +0900

    초기 세팅

---
+++ src/test/java/security/tutorials/TutorialsApplicationTests.java
@@ -0,0 +1,13 @@
+package security.tutorials;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorialsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
